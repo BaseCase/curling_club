9deda759118cd7de8c8f6dc35d78e1d99d7efbe6
Casey Brant <casey@caseybrant.com>
2019-01-16 20:42:51 -0600

    indent 4 instead of 2

--- src/club.swift
+++ src/club.swift
@@ -3,9 +3,9 @@ import Darwin.ncurses
 
 
 struct Junk {
-  var terminal_width: Int32 = 0
-  var terminal_height: Int32 = 0
-  var midpoint: Int32 = 0
+    var terminal_width: Int32 = 0
+    var terminal_height: Int32 = 0
+    var midpoint: Int32 = 0
 }
 
 
@@ -13,34 +13,34 @@ let pipe_char: UInt32 = "|".unicodeScalars.map({ $0.value })[0]
 
 
 func main() {
-  var bucket = Junk()
+    var bucket = Junk()
 
-  initscr()
-  nodelay(stdscr, false)
-  clear()
-  refresh()
+    initscr()
+    nodelay(stdscr, false)
+    clear()
+    refresh()
 
-  bucket.terminal_width = getmaxx(stdscr)
-  bucket.terminal_height = getmaxy(stdscr)
-  bucket.midpoint = bucket.terminal_width / 2
+    bucket.terminal_width = getmaxx(stdscr)
+    bucket.terminal_height = getmaxy(stdscr)
+    bucket.midpoint = bucket.terminal_width / 2
 
-  let left_win = newwin(bucket.terminal_height, bucket.midpoint - 1, 0, 0)
-  waddstr(left_win, "Heyo")
-  wrefresh(left_win)
+    let left_win = newwin(bucket.terminal_height, bucket.midpoint - 1, 0, 0)
+    waddstr(left_win, "Heyo")
+    wrefresh(left_win)
 
-  let right_win = newwin(bucket.terminal_height, bucket.midpoint - 1, 0, bucket.midpoint + 1)
-  waddstr(right_win, "Righty!")
-  wrefresh(right_win)
+    let right_win = newwin(bucket.terminal_height, bucket.midpoint - 1, 0, bucket.midpoint + 1)
+    waddstr(right_win, "Righty!")
+    wrefresh(right_win)
 
-  move(0, bucket.midpoint)
-  vline(pipe_char, bucket.terminal_height)
+    move(0, bucket.midpoint)
+    vline(pipe_char, bucket.terminal_height)
 
 
-  refresh()
+    refresh()
 
-  getch()
+    getch()
 
-  endwin()
+    endwin()
 }
 
 main()
