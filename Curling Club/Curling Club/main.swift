03f8d7fddc2198ec4a0525a87e4fe68eb3d958cd
Casey Brant <casey@caseybrant.com>
2020-04-13 11:41:16 -0500

    get very basic curses program working in xcode proj

--- Curling Club/Curling Club/main.swift
+++ Curling Club/Curling Club/main.swift
@@ -1,5 +1,25 @@
+import Darwin.ncurses
+
+
 func main() {
-    print("Hello, World!")
+    initscr()
+    clear()
+    
+    noecho()
+    curs_set(0)
+    
+    let scr_w = Int(getmaxx(stdscr))
+    let scr_h = Int(getmaxy(stdscr))
+    
+    let message = "Join the club."
+    let x = scr_w / 2 - message.count / 2
+    let y = scr_h / 2
+    move(Int32(y), Int32(x))
+    addstr(message)
+    refresh()
+    
+    getchar()
+    endwin()
 }
 
 
